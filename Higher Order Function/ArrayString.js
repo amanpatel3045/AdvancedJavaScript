71a67291390785f791671419e275df4e209f3e2b
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2022-12-25 21:06:48 +0530

    Delete ArrayString.js

--- Higher Order Function/ArrayString.js
+++
@@ -1,8 +0,0 @@
-function arrstrptr(x) {
-    for (var i = 0; i < x.length; i++) {
-        if (x[i] == "a") {
-            console.log(x[i]);
-        }
-    }
-}
-var a = arrstrptr(["a", "problem", "a", "upload"]);
